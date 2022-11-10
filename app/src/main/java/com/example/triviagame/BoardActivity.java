01aaca1d6635d18c0c0c7729d5c454f18d440d72
lironshish <lironshish1@gmail.con>
2022-11-10 15:58:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/triviagame/BoardActivity.java
@@ -0,0 +1,28 @@
+package com.example.triviagame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class BoardActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private CardView card_question, card_answer_A, card_answer_B, card_answer_C, card_answer_D;
+    private TextView text_question, text_answer_A, text_answer_B, text_answer_C, text_answer_D;;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+        initViews();
+
+    }
+
+    private void initViews() {
+        toolbar = findViewById(R.id.toolbar);
+        
+    }
+}
