b807e34e13c19dfe5e0a8dfb53ed7ece992b6900
lironshish <lironshish1@gmail.con>
2022-11-26 15:30:17 +0200

    My Objects

---
+++ app/src/main/java/com/example/triviagame/Objects/Question.java
@@ -0,0 +1,87 @@
+package com.example.triviagame.Objects;
+
+import java.util.ArrayList;
+
+public class Question {
+    private String category;
+    private String type;
+    private String difficulty;
+    private String question;
+    private String correct_answer;
+    private ArrayList<String> incorrect_answers;
+
+
+    public Question(String category, String type, String difficulty, String question, String correct_answer, ArrayList<String> incorrect_answers) {
+        this.category = category;
+        this.type = type;
+        this.difficulty = difficulty;
+        this.question = question;
+        this.correct_answer = correct_answer;
+        this.incorrect_answers = incorrect_answers;
+    }
+
+    public Question(){
+
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCorrect_answer() {
+        return correct_answer;
+    }
+
+    public void setCorrect_answer(String correct_answer) {
+        this.correct_answer = correct_answer;
+    }
+
+
+    public ArrayList<String> getIncorrect_answers() {
+        return incorrect_answers;
+    }
+
+    public void setIncorrect_answers(ArrayList<String> incorrect_answers) {
+        this.incorrect_answers = incorrect_answers;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "category='" + category + '\'' +
+                ", type='" + type + '\'' +
+                ", difficulty='" + difficulty + '\'' +
+                ", question='" + question + '\'' +
+                ", correct_answer='" + correct_answer + '\'' +
+                ", incorrect_answers=" + incorrect_answers +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/triviagame/Objects/Results.java
@@ -0,0 +1,45 @@
+package com.example.triviagame.Objects;
+
+import java.util.ArrayList;
+
+public class Results {
+
+    private int response_code;
+    private ArrayList<Question> results;
+
+    public Results(int response_code, ArrayList<Question> results) {
+        this.response_code = response_code;
+        this.results = results;
+    }
+
+    public Results(){
+        results = new ArrayList<>();
+    }
+
+    public int getResponse_code() {
+        return response_code;
+    }
+
+    public void setResponse_code(int response_code) {
+        this.response_code = response_code;
+    }
+/*
+
+    public Question[] getResults() {
+        return results;
+    }
+
+    public void setResults(Question[] results) {
+        this.results = results;
+    }
+
+*/
+
+    @Override
+    public String toString() {
+        return "MyJson{" +
+                "response_code=" + response_code +
+                ", results=" + results.toString() +
+                '}';
+    }
+}
